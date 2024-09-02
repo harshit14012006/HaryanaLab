45924cd26ab413f6d3345edc6f1cbc6c67a40a06
harshit6002 <harshitemail2006@gmail.com>
2024-09-02 09:41:47 +0530

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import Layout from './Layout'
-import LedgerReport from './pages/LedgerReport';
-import CashVoucher from './pages/CashVoucher';
 function App() {
   return (
     <div>
       <Layout />
-      {/* <CashVoucher/> */}
-      {/* <LedgerReport/> */}
     </div>
   )
 }
