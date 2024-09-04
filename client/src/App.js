dc8c515306ccabc44f91f55670adc01cfb801158
harshit6002 <harshitemail2006@gmail.com>
2024-09-04 17:19:37 +0530

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Layout from "./Layout";
 import LabReport from "./pages/LabReport"; 
-import LabReport1 from './pages/LabReport1'
 function App() {
   return (
     <div>
