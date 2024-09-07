be44c7600dfd94bb3ef40643d60edda7f5f0c529
harshit6002 <harshitemail2006@gmail.com>
2024-09-07 13:17:17 +0530

    ui all done

--- client/src/pages/CashVoucher.jsx
+++ client/src/pages/CashVoucher.jsx
@@ -8,14 +8,14 @@ function CashVoucher() {
   };
 
   return (
-    <div className="bg-gray-100 flex justify-between  box-border ">
-      <div className="flex-1 mx-2 bg-gray-100 p-3  h-full  rounded-lg flex flex-col items-center">
-        <div className="border p-4 rounded-lg border-gray-300 font-bold w-full max-w-lg text-center text-4xl mb-8">
+    <div className="box-border flex justify-between bg-gray-100 ">
+      <div className="flex flex-col items-center flex-1 h-full p-3 mx-2 bg-gray-100 rounded-lg">
+        <div className="w-full max-w-lg p-4 mb-8 text-4xl font-bold text-center border border-gray-300 rounded-lg">
           Cash Voucher
         </div>
         <form onSubmit={handleSubmit} className="w-full max-w-lg">
-          <fieldset className="border border-gray-300 rounded-lg p-2 h-28 box-border mb-3">
-            <legend className="font-normal mb-4">Day Detail</legend>
+          <fieldset className="box-border p-2 mb-3 border border-gray-300 rounded-lg h-28">
+            <legend className="mb-4 font-normal">Day Detail</legend>
             <div className="flex items-center mb-4">
               <label className="mr-4 font-normal whitespace-nowrap min-w-[120px]">
                 Date:
@@ -23,13 +23,13 @@ function CashVoucher() {
               <input 
                 type="date" 
                 required 
-                className="flex-1 p-2 rounded-md w-full border h-8 border-gray-300 box-border" 
+                className="box-border flex-1 w-full h-8 p-2 border border-gray-300 rounded-md" 
                 min={new Date().toISOString().split('T')[0]} 
               />
             </div>
           </fieldset>
-          <fieldset className="border border-gray-300 rounded-lg p-6 box-border mb-8">
-            <legend className="font-normal mb-4">Voucher Details</legend>
+          <fieldset className="box-border p-6 mb-8 border border-gray-300 rounded-lg">
+            <legend className="mb-4 font-normal">Voucher Details</legend>
             <div className="flex items-center mb-2">
               <label className="mr-4 font-normal whitespace-nowrap min-w-[120px]">
                 Party Name:
@@ -37,7 +37,7 @@ function CashVoucher() {
               <div className="relative flex-1">
                 <select 
                   required 
-                  className="appearance-none p-1 rounded-md w-full h-8 border border-gray-300 box-border cursor-pointer">
+                  className="box-border w-full h-8 p-1 border border-gray-300 rounded-md appearance-none cursor-pointer">
                   <option value="">Select Party</option>
                   <option value="party1">Party 1</option>
                   <option value="party2">Party 2</option>
@@ -57,7 +57,7 @@ function CashVoucher() {
               <input 
                 type="number" 
                 required 
-                className="flex-1 p-2 rounded-md h-8 w-full border border-gray-300 box-border" 
+                className="box-border flex-1 w-full h-8 p-2 border border-gray-300 rounded-md" 
                 min="0" 
               />
             </div>
@@ -68,28 +68,30 @@ function CashVoucher() {
               <input 
                 type="text" 
                 required 
-                className="flex-1 p-2 rounded-md w-full h-8 border border-gray-300 box-border" 
+                className="box-border flex-1 w-full h-8 p-2 border border-gray-300 rounded-md" 
                 pattern=".{3,}" 
                 title="Remarks should be at least 3 characters long" 
               />
             </div>
           </fieldset>
           <div className="flex justify-between w-full">
-            <button type="submit" className="px-6 py-1 h-8 rounded-md border-none bg-gray-300 font-bold cursor-pointer">
+            <button type="submit" className="h-8 px-6 py-1 font-bold bg-gray-300 border-none rounded-md cursor-pointer">
               Add
             </button>
-            <button type="button" className="px-6 py-1 h-8 rounded-md border-none bg-gray-300 font-bold cursor-pointer">
+            <button type="button" className="h-8 px-6 py-1 font-bold bg-gray-300 border-none rounded-md cursor-pointer">
               Delete
             </button>
+
+            
           </div>
         </form>
       </div>
-      <div className="flex-1 mx-2 bg-gray-100 p-3 box-border rounded-lg flex flex-col items-center">
+      <div className="box-border flex flex-col items-center flex-1 p-3 mx-2 bg-gray-100 rounded-lg">
         <div className="w-full">
-          <u><h1 className="text-left mb-1">Party:</h1></u>
+          <u><h1 className="mb-1 text-left">Party:</h1></u>
         </div>
         <div className="w-full">
-          <u><h1 className="text-left mb-2">City:</h1></u>
+          <u><h1 className="mb-2 text-left">City:</h1></u>
         </div>
         <div className="w-full">
           <h7 className="text-left">Day Cash Details</h7>
